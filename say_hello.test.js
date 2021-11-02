41ea3df9cab0daf389abeb5fe9617a3ec39e03e4
Jns234 <59916720+Jns234@users.noreply.github.com>
2021-11-02 09:04:21 +0200

    added jest to eslint

--- say_hello.test.js
+++ say_hello.test.js
@@ -1,5 +1,5 @@
-const sayHello = require('./say_hello');
+const sayHello = require("./say_hello");
 
 test("return 'Hello Jan!' if given input Jan", () => {
-    expect(sayHello("jan")).toBe("Hello Jan!");
-})
+  expect(sayHello("jan")).toBe("Hello Jan!");
+});
