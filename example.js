81b7dc520e2fbdd13dd9515f66bb03be71849039
Ingela <59916738+ingeliza@users.noreply.github.com>
2021-11-02 08:30:51 +0200

    updated Kaja Kallas to Kaia

--- example.js
+++ example.js
@@ -8,11 +8,11 @@ const say_hello = require("./say_hello.js")
 console.log(say_hello("jaan"));
 // use also colors to be more "fancy"
 console.log(chalk.yellow('mari: '), chalk.bold.green(say_hello("mari")))
-console.log(chalk.yellow(`kaja kallas: `), chalk.bold.green(say_hello("kaja kallas")));
+console.log(chalk.yellow(`kaia: `), chalk.bold.green(say_hello("kaia")));
 
 
 // examples of testing using assert
 assert.equal(say_hello("mari"), 'Hello Mari!');
-assert.equal(say_hello("kaja kallas"), 'Hello Kaja Kallas!')
+assert.equal(say_hello("kaia"), 'Hello Kaia!')
 
 assert.notEqual(say_hello("mati"), 'bye mati')
