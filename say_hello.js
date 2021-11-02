0294389c14aead5f93ff404f6ce0fd8eff360c75
AndresTPT <Andres.Tammistu@tptlive.ee>
2021-11-02 09:25:17 +0200

    eslint

--- say_hello.js
+++ say_hello.js
@@ -1,7 +1,11 @@
 var capitalize = require('capitalize');
 
-function sayHello(name) {
-    return "Hello " + capitalize.words(name) + "!";
+function sayHello(Andres) {
+    return "Hello " + capitalize.words(Andres) + "!";
 }
+console.log('repeat("a", 5): ', repeat("a", 5));
+console.log("repeat(\"a\", 0): ", repeat('a', 0));
+
+console.log(`repeat("testme ", 2): `, repeat("testme", 2));
 
 module.exports = sayHello
