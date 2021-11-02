a8523ab2ea6437dd17190416734b70c17aef0247
Ingela <59916738+ingeliza@users.noreply.github.com>
2021-11-02 09:33:42 +0200

    enforced no-undef to be off

--- say_hello.test.js
+++ say_hello.test.js
@@ -1,13 +1,11 @@
 const sayHello = require("./say_hello");
 
-
 test("name mari will be Mari", () => {
-    const name="mari";
-    expect(sayHello(name)).toBe("Hello Mari!")
-})
-
+  const name = "mari";
+  expect(sayHello(name)).toBe("Hello Mari!");
+});
 
 test("name mari mets will be Mari Mets", () => {
-    const name="mari mets";
-    expect(sayHello(name)).toBe("Hello Mari Mets!")
-})
+  const name = "mari mets";
+  expect(sayHello(name)).toBe("Hello Mari Mets!");
+});
