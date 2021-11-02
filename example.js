19565d1b7aed0ce45417452140ac98f4d9f59f1e
Your Name <asko.magi@tptlive.ee>
2021-11-02 08:46:25 +0200

    Fixed a warning in example.js

--- example.js
+++ example.js
@@ -1,8 +1,7 @@
 const chalk = require("chalk");
 const assert = require("assert");
 
-// eslint-disable-next-line import/extensions
-const say_hello = require("./say_hello.js");
+const say_hello = require("./say_hello");
 
 // test how function works
 console.log(say_hello("jaan"));
