a0cb4084bae4b001dc6d4f94619c8dc45efd0ed9
sink <siim.sink@tptlive.ee>
2021-11-02 09:39:13 +0200

    eslint, jest, test

--- say_hello.js
+++ say_hello.js
@@ -1,7 +1,11 @@
 var capitalize = require('capitalize');
 
-function sayHello(name) {
-    return "Hello " + capitalize.words(name) + "!";
+function sayHello(Siim) {
+    return "Hello " + capitalize.words(Siim) + "!";
 }
+
+console.log('repeat("a", 5): ', repeat("a", 5));
+console.log("repeat(\"a\", 0): ", repeat('a', 0));
+console.log(`repeat("testme ", 2): `, repeat("testme", 2));
 
 module.exports = sayHello
